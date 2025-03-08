993521d4088f5b6a62faad7303dd73a4b4cd7cac
Yukari Chess <yukarichess@gmail.com>
2025-03-08 00:49:20 +0000

    Alternate LMP formula
    Passed STC:
    Elo   | 4.30 +- 3.35 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=16MB
    LLR   | 2.90 (-2.25, 2.89) [0.00, 5.00]
    Games | N: 15500 W: 4316 L: 4124 D: 7060
    Penta | [258, 1777, 3493, 1959, 263]
    
    Passed LTC:
    Elo   | 9.55 +- 5.47 (95%)
    SPRT  | 40.0+0.40s Threads=1 Hash=128MB
    LLR   | 2.93 (-2.25, 2.89) [0.00, 5.00]
    Games | N: 4878 W: 1294 L: 1160 D: 2424
    Penta | [39, 537, 1161, 655, 47]
    
    bench: 4878822

--- yukari/src/search.rs
+++ yukari/src/search.rs
@@ -478,7 +478,7 @@ impl<'a> Search<'a> {
                 }
             }
             
-            let lmp_threshold = 1.max(if depth == 1 { (3 * moves.len()) / 4 } else { (7 * moves.len()) / 8 });
+            let lmp_threshold = 5 + (depth as usize).pow(2);
             if !board.in_check() && !m.is_capture() && depth <= 2 && movecount >= lmp_threshold && best_score > -MATE_VALUE + 500 {
                 continue;
             }
