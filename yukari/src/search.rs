559397fafbb70705a052ba03e72d7cec7d40a361
Yukari Chess <yukarichess@gmail.com>
2025-03-10 02:36:56 +0000

    SEE Pruning at depth 5
    Passed STC:
    Elo   | 9.14 +- 5.53 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=16MB
    LLR   | 3.04 (-2.25, 2.89) [0.00, 5.00]
    Games | N: 6272 W: 1850 L: 1685 D: 2737
    Penta | [122, 698, 1361, 803, 152]
    
    Passed LTC:
    Elo   | 3.39 +- 2.72 (95%)
    SPRT  | 40.0+0.40s Threads=1 Hash=128MB
    LLR   | 2.92 (-2.25, 2.89) [0.00, 5.00]
    Games | N: 20590 W: 5375 L: 5174 D: 10041
    Penta | [183, 2494, 4786, 2603, 229]
    
    bench: 4067248

--- yukari/src/search.rs
+++ yukari/src/search.rs
@@ -467,7 +467,7 @@ impl<'a> Search<'a> {
             }
 
             // SEE Pruning
-            if !board.in_check() && (2..=4).contains(&depth) && movecount > 1 && best_score > -MATE_VALUE + 500 {
+            if !board.in_check() && (2..=5).contains(&depth) && movecount > 1 && best_score > -MATE_VALUE + 500 {
                 let see = board.static_exchange_evaluation(m);
                 if m.is_capture() && see < -1 {
                     continue;
