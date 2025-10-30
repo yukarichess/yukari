6b92dfcbfaffd0a88708cedf583d1bb73e6feeae
KazApps <kazuki_shogi@outlook.jp>
2025-10-30 19:01:23 +0900

    Add x86_64 prefetch.
    Passed STC:
    Elo   | 2.93 +- 2.34 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=16MB
    LLR   | 2.95 (-2.25, 2.89) [0.00, 5.00]
    Games | N: 21332 W: 4279 L: 4099 D: 12954
    Penta | [108, 2143, 6006, 2279, 130]
    
    Passed LTC:
    Elo   | 4.82 +- 3.32 (95%)
    SPRT  | 40.0+0.40s Threads=1 Hash=128MB
    LLR   | 2.92 (-2.25, 2.89) [0.00, 5.00]
    Games | N: 8724 W: 1664 L: 1543 D: 5517
    Penta | [16, 777, 2659, 890, 20]
    
    bench: 5531990

--- yukari/src/search.rs
+++ yukari/src/search.rs
@@ -3,6 +3,9 @@ use std::{cmp::Ordering, sync::atomic::AtomicU64, time::Instant};
 use tinyvec::ArrayVec;
 use yukari_movegen::{Board, Colour, Move, Piece};
 
+#[cfg(target_arch = "x86_64")]
+use core::arch::x86_64::{_mm_prefetch, _MM_HINT_T0};
+
 const MATE_VALUE: i32 = 10_000;
 
 #[derive(Clone)]
@@ -442,6 +445,18 @@ impl<'a> Search<'a> {
         None
     }
 
+    #[cfg(target_arch = "x86_64")]
+    #[inline(always)]
+    fn prefetch_tt(&self, board: &Board, m: Move) {
+        let entry = (board.hash_after(m) & ((self.tt.len() - 1) as u64)) as usize;
+        let entry = &self.tt[entry];
+        unsafe { _mm_prefetch::<_MM_HINT_T0>(entry as *const _ as *const i8) }
+    }
+
+    #[cfg(not(target_arch = "x86_64"))]
+    #[inline(always)]
+    fn prefetch_tt(&self, board: &Board, m: Move) {}
+
     fn write_tt(&self, board: &Board, ply: i32, mut data: TtData) {
         let entry = (board.hash() & ((self.tt.len() - 1) as u64)) as usize;
         let entry = &self.tt[entry];
@@ -716,6 +731,8 @@ impl<'a> Search<'a> {
                     extension += 1;
                 }
             }
+
+            self.prefetch_tt(board, m);
 
             self.path.push(Some((board.piece_from_square(m.from).unwrap(), m)));
 
