b5aade8cedc1fd80fceb72edb6af798a47c093dc
Yukari Chess <yukarichess@gmail.com>
2025-11-19 21:43:00 +0000

    Reverse Futility Pruning at depth 8
    Passed STC:
    Elo   | 7.35 +- 4.45 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=16MB
    LLR   | 2.92 (-2.25, 2.89) [0.00, 5.00]
    Games | N: 5670 W: 1494 L: 1374 D: 2802
    Penta | [50, 410, 1813, 494, 68]
    
    Passed LTC:
    Elo   | 3.31 +- 2.60 (95%)
    SPRT  | 40.0+0.40s Threads=1 Hash=128MB
    LLR   | 2.94 (-2.25, 2.89) [0.00, 5.00]
    Games | N: 17846 W: 4475 L: 4305 D: 9066
    Penta | [169, 1529, 5397, 1619, 209]
    
    bench: 3649645

--- yukari/src/search.rs
+++ yukari/src/search.rs
@@ -248,7 +248,7 @@ impl Thread {
 
         let eval = self.board[ply].eval(self.board[ply].side());
         let rfp_margin = 45 * depth;
-        if !self.board[ply].in_check() && depth <= 7 && eval - rfp_margin >= beta {
+        if !self.board[ply].in_check() && depth <= 8 && eval - rfp_margin >= beta {
             return eval - rfp_margin;
         }
 
