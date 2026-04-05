fa3e1ce82b7eb4d36edc27879331c3817ad173eb
KazApps <kazuki_shogi@outlook.jp>
2026-04-05 13:47:48 +0900

    Low depth singular extension bench: 987791

--- yukari/src/search.rs
+++ yukari/src/search.rs
@@ -499,6 +499,9 @@ impl Thread {
                     if score < singular_beta {
                         extension += 1;
                     }
+                // Low depth singular extension: Determine singularity by static eval vs alpha.
+                } else if !self.board[ply].in_check() && depth <= 7 && eval <= alpha - 25 && tt_entry.flags == TtFlags::Lower {
+                    extension += 1;
                 }
             }
 
