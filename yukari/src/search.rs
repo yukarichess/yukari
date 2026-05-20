6a8727e5cff6e7f02ba666c5579831b72b9f742d
87 <dev@87flowers.com>
2026-05-20 16:52:33 +0200

    Bench: 2629383

--- yukari/src/search.rs
+++ yukari/src/search.rs
@@ -559,6 +559,9 @@ impl Thread {
                     // The TT move seems uniquely good; extend.
                     if score < singular_beta {
                         extension += 1;
+                        if !expected_pvnode && score < singular_beta - 20 && tt_entry.depth as i32 >= depth - 2 {
+                            extension += 1;
+                        }
                     } else if tt_entry.score as i32 >= beta {
                         extension -= 1;
                     }
