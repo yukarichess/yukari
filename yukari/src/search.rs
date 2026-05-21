41f54b5bc9d822f1860ecfd634f9aa796aac1213
Yukari Chess <yukarichess@gmail.com>
2026-05-21 12:06:51 +0100

    lily beta probcut
    bench: 2813576

--- yukari/src/search.rs
+++ yukari/src/search.rs
@@ -477,21 +477,13 @@ impl Thread {
             }
         }
 
-        if excluded_move.is_none() && !expected_pvnode && !self.board[ply].in_check() && depth >= 2 && eval >= beta {
-            self.keystack.push(self.board[ply].hash());
-            if self.board.len() <= ply + 1 {
-                self.board.push(self.board[ply].make_null());
-            } else {
-                self.board[ply + 1] = self.board[ply].make_null();
-            }
-            self.path.push(None);
-            let reduction = if depth > 6 { 4 } else { 3 };
-            let score = -self.search(depth - 1 - reduction, -beta, -beta + 1, ply + 1, tt, None);
-            self.path.pop();
-            self.keystack.pop();
+        if excluded_move.is_none() && !expected_pvnode && !self.board[ply].in_check() && depth >= 8 && eval >= beta {
+            let reduction = depth / 2;
+            let bound = beta + 80 + depth * 5;
+            let score = self.search(depth - 1 - reduction, bound - 1, bound, ply, tt, None);
 
-            if score >= beta {
-                return score;
+            if score >= bound {
+                return beta;
             }
         }
 
