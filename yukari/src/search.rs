d7be593f6922689be360f514ba8f265b20491019
Yukari Chess <yukarichess@gmail.com>
2026-07-07 15:13:15 +0100

    Increase singular negative extension if non-PV
    bench: 1962754

--- yukari/src/search.rs
+++ yukari/src/search.rs
@@ -700,7 +700,7 @@ impl Thread {
                             extension += i32::from(score < singular_beta - self.params.singular_triple_margin);
                         }
                     } else if tt_entry.score as i32 >= beta || tt_entry.score as i32 <= alpha {
-                        extension -= 1;
+                        extension -= if expected_pvnode { 1 } else { 2 };
                     }
                 // Low depth singular extension: Determine singularity by static eval vs alpha.
                 } else if !self.board[ply].in_check()
